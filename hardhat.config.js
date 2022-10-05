058bae0717a572bd381533195987d1dc468bd856
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-05 16:33:48 +0600

    19-Hardhat Config Updated with networks Section
    Here I have added the `networks` section at `haedhat.config.js`. Configured the network section with Ganache local Ethereum network and named the network attribute as `localganache`

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,16 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-waffle");
+
+// Index: 3 || Address: 0xC7dC9Fa47563985AC25445D2697e964315460868
+const GANACHE_PRIVATE_KEY = "ac47c7d4cabfc6b63ab9d6836785d44cf4d5e36e273f8c8abc6fcc02873dcbf9";
+const GANACHE_URL = "http://localhost:7545";
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    localganache: {
+      url: GANACHE_URL,
+      accounts: [`0x${GANACHE_PRIVATE_KEY}`],
+    },
+  },
 };
