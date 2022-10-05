4e63d4803db7b5d550282f0b0f012223aaefa96b
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-05 07:05:29 +0600

    17-AbstractOperationalActors Created

---
+++ contracts/utils/AbstractOperationalActors.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+/**
+ * Provides address of the current executing actor, including the
+ * sender of the transaction.
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract AbstractOperationalActors {
+
+}
