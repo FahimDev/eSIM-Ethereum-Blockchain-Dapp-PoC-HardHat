3138a4c4b395b37db64a4a07f495e59fdc76c903
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-06 18:28:00 +0600

    21-ISIM gets implemented at EmbeddedSIM Abstract Class

--- contracts/abstracts/EmbeddedSIM.sol
+++ contracts/abstracts/EmbeddedSIM.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
+import "../interfaces/ISIM.sol";
+
 /**
  * Embedded SIM or eSIM is a form of programmable SIM card 
  * that is embedded directly into a device.
  */
-abstract contract EmbeddedSIM {
+abstract contract EmbeddedSIM is ISIM {
 
 }
