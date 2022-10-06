7e794aebd8ec4876a4eb29ce4d48027ff457ddab
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-06 18:15:12 +0600

    21-eSIM Abstract Contract Created

---
+++ contracts/abstracts/embeddedSIM.abstract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+/**
+ * Embedded SIM or eSIM is a form of programmable SIM card 
+ * that is embedded directly into a device.
+ */
+abstract contract EmbeddedSIM {
+
+}
