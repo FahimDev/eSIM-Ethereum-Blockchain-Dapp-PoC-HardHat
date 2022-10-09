7955cdfb14541968ff9cd0a419a7bd4390457b36
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-09 11:58:14 +0600

    23-Dependency Injection deployed to Ganache Net
    Successfully Deployed two more Contracts at Ganachi Network where USIM Contract's dependency has been Injected to the constructor of EmbeddedSIMManager.
    
    As our Ganachi deploy script is written in JavaScript and USIM constructor requires Struct in Solidity. I have created a JS object structure for this temporary test and passed that JS Structure at USIM constructor.
    And Deployed USIM Contract successfully.
    
    Here, I have contributed the knowledge to the community: (1) https://ethereum.stackexchange.com/questions/120566/how-do-i-pass-in-an-array-of-struct-from-js-to-a-contract/137124#137124
    (2) https://stackoverflow.com/questions/502366/structs-in-javascript/74002397#74002397

--- contracts/USIM.sol
+++ contracts/USIM.sol
@@ -9,16 +9,15 @@ import "./utils/AbstractOperationalActors.sol";
 
 /**
  * The SIM card which support's 2G and 3G both type of network is
- * known as USIM. This SIM's UICC is identified with its ICCID 
- * (Integrated Circuit Card IDentification). The ICCID specifies 
+ * known as USIM. This SIM's UICC is identified with its ICCID
+ * (Integrated Circuit Card IDentification). The ICCID specifies
  * maximum of 19 digits where First two digits are Reserved for Telecommunication,
- * next 1-3 digits are MCC number, next Issuer Identifier Numbers are allocated by 
+ * next 1-3 digits are MCC number, next Issuer Identifier Numbers are allocated by
  * the National Administration then comes the serial number and the ending is the check digit.
  * MCC: Mobile Country Code
  * MNC: Mobile Network Code
  */
 contract USIM is ISIM {
-
     uint private _iccid;
     MNOCommunicationProfile private _mnoCommunicationProfile;
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,10 +9,36 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  // JS Structure passed as Solidity Struct data type
+  // Also contributed to community: (1) https://ethereum.stackexchange.com/questions/120566/how-do-i-pass-in-an-array-of-struct-from-js-to-a-contract/137124#137124
+  // (2) https://stackoverflow.com/questions/502366/structs-in-javascript/74002397#74002397
+  var structJS = {
+    id: 55,
+    title: "Grameenphone",
+    iccid: "56",
+    msisdn: "17100000000",
+    imsi: "12345",
+  };
+  // Ref: https://docs.openzeppelin.com/learn/deploying-and-interacting
   const GenesisContract = await ethers.getContractFactory("SIMTokenization");
+  const USIMContract = await ethers.getContractFactory("USIM");
+  const ESIMManagerContract = await ethers.getContractFactory(
+    "EmbeddedSIMManager"
+  );
+  // Hardhat doesn’t keep track of your deployed contracts.
+  // We displayed the deployed address in our script
   const genesisContract = await GenesisContract.deploy();
+  const usimContract = await USIMContract.deploy(89, structJS);
+  const esimManagerContract = await ESIMManagerContract.deploy(
+    usimContract.address
+  );
+  // Checking Timeout and Smart Contract Timeout Status.
   await genesisContract.deployed();
-  console.log("Deployer Address: ", genesisContract.address);
+  await usimContract.deployed();
+  await esimManagerContract.deployed();
+  console.log("Genesis deployed to: ", genesisContract.address);
+  console.log("USIM deployed to: ", usimContract.address);
+  console.log("ESIM deployed to: ", esimManagerContract.address);
 }
 
 main()
