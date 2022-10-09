0fa79a4eabfe87c336ee8b18e4e21606937faf65
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-09 09:19:00 +0600

    23-Dependency Injection Implemented at EmbeddedSIMManager

--- contracts/EmbeddedSIMManager.sol
+++ contracts/EmbeddedSIMManager.sol
@@ -6,12 +6,19 @@ import "./abstracts/EmbeddedSIM.sol";
 
 contract EmbeddedSIMManager is EmbeddedSIM {
 
+    address private _iSIMAddress;
+    
+    // Adding Dependency Injection of USIM Contract in this contract (EmbeddedSIMManager).
+    constructor (address iSIMAddress_) {
+        _iSIMAddress = iSIMAddress_;
+    }
+
     function getCardUniqueIds() external view override returns (string memory){
 
     }
 
     function getCardType(uint _cardUniqueId) external view override returns (SIMType){
-
+        return ISIM(_iSIMAddress).getCardType(_cardUniqueId);
     }
 
     function enableMNOProfile(uint _mnoId) external view override returns (bool){
