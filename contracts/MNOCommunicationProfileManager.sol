65e18ad7b8a4ebbab06493ce032a57955c599b1a
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-12 16:05:44 +0600

    29-MNOCommunicationProfileManager Contract Refactored for Integration

--- contracts/MNOCommunicationProfileManager.sol
+++ contracts/MNOCommunicationProfileManager.sol
@@ -6,31 +6,22 @@ import "hardhat/console.sol";
 import "./dto/CircuitCardDTO.sol";
 
 contract MNOCommunicationProfileManager {
-
     string public contractTitle;
     string public contractSymbol;
-    uint public mnoID; // MNO ID will act as a Foreign Key to the Mobie Network Operator details
-    uint public mcc; // MCC - Mobile Country Code
-    uint public mnc; //  MNC - Mobile Network Code
-    address private _owner;
+    address public owner;
     MNOCommunicationProfile[] private _mnoCommunicarionProfiles;
 
-    constructor(
-        string memory contractTitle_,
-        string memory contractSymbol_,
-        uint mnoID_,
-        uint mcc_,
-        uint mnc_  
-    ) {
+    constructor(string memory contractTitle_, string memory contractSymbol_) {
         contractTitle = contractTitle_;
         contractSymbol = contractSymbol_;
-        mnoID = mnoID_;
-        mcc = mcc_;
-        mnc = mnc_;
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
-    function showAllCommunicationProfile() external view returns (MNOCommunicationProfile[] memory) {
+    function showAllCommunicationProfile()
+        external
+        view
+        returns (MNOCommunicationProfile[] memory)
+    {
         return _mnoCommunicarionProfiles;
     }
 
@@ -39,7 +30,10 @@ contract MNOCommunicationProfileManager {
         view
         returns (MNOCommunicationProfile memory)
     {
-        MNOCommunicationProfile memory _targetCommunicationProfile = _mnoCommunicarionProfiles[_communicationProfileIndexId];
+        MNOCommunicationProfile
+            memory _targetCommunicationProfile = _mnoCommunicarionProfiles[
+                _communicationProfileIndexId
+            ];
         return _targetCommunicationProfile;
     }
 
@@ -50,18 +44,27 @@ contract MNOCommunicationProfileManager {
         string memory _imsi
     ) external returns (MNOCommunicationProfile memory) {
         MNOCommunicationProfile memory _communicationProfileTempPayload;
-        _communicationProfileTempPayload.id = _mnoCommunicarionProfiles.length + 1;
+        _communicationProfileTempPayload.id =
+            _mnoCommunicarionProfiles.length +
+            1;
         _communicationProfileTempPayload.title = _title;
         _communicationProfileTempPayload.iccid = _iccid;
         _communicationProfileTempPayload.msisdn = _msisdn;
         _communicationProfileTempPayload.imsi = _imsi;
-        uint indexId = addMNOCommunicationProfile(_communicationProfileTempPayload);
-        console.log("==> DEBUG: Pushed Communication Profile Index Number: ", indexId);
+        uint indexId = addMNOCommunicationProfile(
+            _communicationProfileTempPayload
+        );
+        console.log(
+            "==> DEBUG: Pushed Communication Profile Index Number: ",
+            indexId
+        );
         return _communicationProfileTempPayload;
     }
 
-    function addMNOCommunicationProfile(MNOCommunicationProfile memory _mnoCommunicarionProfile) internal returns (uint) {
+    function addMNOCommunicationProfile(
+        MNOCommunicationProfile memory _mnoCommunicarionProfile
+    ) internal returns (uint) {
         _mnoCommunicarionProfiles.push(_mnoCommunicarionProfile);
-        return _mnoCommunicarionProfiles.length;
+        return _mnoCommunicarionProfiles.length - 1;
     }
 }
