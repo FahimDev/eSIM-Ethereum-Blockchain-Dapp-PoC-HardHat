e687f7d9ac75178869319ebc9f133905f81aefa4
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-16 08:47:43 +0600

    33-Swagger tools suite Installed for API development and test

--- esim-client/pages/api/hello.ts
+++ esim-client/pages/api/hello.ts
@@ -5,6 +5,16 @@ type Data = {
   name: string
 }
 
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Returns  John Doe
+ *     responses:
+ *       200:
+ *         description: hello
+ */
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
