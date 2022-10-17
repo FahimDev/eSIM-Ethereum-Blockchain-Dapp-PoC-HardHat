e339cd8badc5a1407831a4a2bba7d1a9f78a7ff2
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-17 13:27:47 +0600

    36-Basic Correction at Landing Page

--- esim-client/pages/index.tsx
+++ esim-client/pages/index.tsx
@@ -14,40 +14,40 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to eSIM Dapp
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Global eSIM DAO and IoT ownership manaement in{' '}
+          <code className={styles.code}>Blockchain</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <button className={styles.card}>
+            <h2>Connect Wallet &rarr;</h2>
+            <p>Get Connected with your MetaMask Wallet.</p>
+          </button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="#" className={styles.card}>
+            <h2>Add Organization &rarr;</h2>
+            <p>Add new Organizations for operating in the DAO system</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="#"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>MNO Profile &rarr;</h2>
+            <p>Discover and deploy new Mobile-Network-Operator and MNO Communication Profiles.</p>
+          </a>
+
+          <a
+            href="https://www.gsma.com/"
+            className={styles.card}
+          >
+            <h2>GSMA &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Representing the worldwide mobile communications industry.
             </p>
           </a>
         </div>
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/bs23.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
