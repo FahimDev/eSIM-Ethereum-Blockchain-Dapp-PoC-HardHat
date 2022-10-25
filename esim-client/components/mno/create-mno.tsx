37a73548da0780b59110b4f6dc4e7a15643a5cd7
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-25 14:30:32 +0600

    36-MNO Registration Form Updated

--- esim-client/components/mno/create-mno.tsx
+++ esim-client/components/mno/create-mno.tsx
@@ -1,3 +1,5 @@
+import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -11,20 +13,110 @@ const CreateMNOComponent: NextPage = () => {
           <div className="basis-3/6">
             <form className="shadow-xl border-double border-4 border-cyan-600 rounded-lg border-x-cyan-100">
               <div className="p-8">
-                <h1 className="capitalize hover:uppercase text-2xl">Register</h1>
+                <h1 className="capitalize hover:uppercase text-2xl">
+                  Register
+                </h1>
                 <p className="indent-4">
                   Please fill in this form to Register a Mobile Network
-                  Operator.
+                  Operator.{" "}
+                  <a
+                    href="https://cellidfinder.com/mcc-mnc"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faCircleQuestion} />
+                  </a>
                 </p>
                 <hr />
                 <label>
-                  <b>Email</b>
+                  <b>Title</b>
                 </label>
                 <input
                   type="text"
-                  placeholder="Enter Email"
-                  name="email"
-                  id="email"
+                  placeholder="Enter Company Title"
+                  name="title"
+                  id="title"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  required
+                />
+                <label>
+                  <b>Brand</b>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Brand Name"
+                  name="brand"
+                  id="brand"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  required
+                />
+                <label>
+                  <b>Network</b>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Network Name"
+                  name="network"
+                  id="network"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  required
+                />
+
+                <label>
+                  <b>Prefix</b>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Enter Operator Prefix"
+                  name="prefix"
+                  id="prefix"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  required
+                />
+                <label>
+                  <b>
+                    Mobile Country Code <sub>(MCC)</sub>
+                  </b>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Enter Mobile Country Code"
+                  name="mcc"
+                  id="mcc"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  required
+                />
+                <label>
+                  <b>
+                    {" "}
+                    Mobile Network Code <sub>(MNC)</sub>
+                  </b>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Enter  Mobile Network Code"
+                  name="mnc"
+                  id="mnc"
                   className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
               focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
               disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -33,9 +125,84 @@ const CreateMNOComponent: NextPage = () => {
                   required
                 />
                 <hr />
+
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    State
+                  </label>
+                  <div className="relative">
+                    <select
+                      className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-state"
+                    >
+                      <option>Active</option>
+                      <option>Inactive</option>
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Username
+                    </label>
+                    <input
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                      id="username"
+                      type="text"
+                      placeholder="<Brand><MNC><OfficeID>"
+                    />
+                    <p className="text-red-500 text-xs italic">
+                      Please fill out this field.
+                    </p>
+                  </div>
+                  <div className="w-full md:w-1/2 px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Email
+                    </label>
+                    <input
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="email"
+                      type="email"
+                      placeholder="example@official.com"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Password
+                    </label>
+                    <input
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="password"
+                      type="password"
+                      placeholder="******************"
+                    />
+                    <p className="text-gray-600 text-xs italic">
+                      Make it as long and as crazy as you'd like
+                    </p>
+                  </div>
+                </div>
+                <hr />
                 <p>
                   By creating an account you agree to our{" "}
-                  <a className="no-underline hover:underline decoration-4 decoration-lime-800" href="#">Terms & Privacy</a>.
+                  <a
+                    className="no-underline hover:underline decoration-4 decoration-lime-800"
+                    href="#"
+                  >
+                    Terms & Privacy
+                  </a>
+                  .
                 </p>
                 <div className="flex items-center justify-center p-4">
                   <span className="relative inline-flex">
@@ -54,7 +221,14 @@ const CreateMNOComponent: NextPage = () => {
 
                 <div className={styles.signin}>
                   <p>
-                    Already have an account? <a className="no-underline hover:underline decoration-4 decoration-sky-500" href="#">Sign in</a>.
+                    Already have an account?{" "}
+                    <a
+                      className="no-underline hover:underline decoration-4 decoration-sky-500"
+                      href="#"
+                    >
+                      Sign in
+                    </a>
+                    .
                   </p>
                 </div>
               </div>
