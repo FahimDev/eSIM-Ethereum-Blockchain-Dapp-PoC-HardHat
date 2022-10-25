a515a89e88fc3a14900a512c3de7b1d33e9a0310
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-25 10:36:37 +0600

    36-Dashboard Layout Created and Integrated at Index as Component
    - Component Repo Created: dashboard
    - Banner Component Created
    - TechCard Component Created
    - Two new Component added at  Index Page
    - Banner and TechCard css modules are created at Style directory
    Design Ref: https://codepen.io/mrReiha

--- esim-client/pages/index.tsx
+++ esim-client/pages/index.tsx
@@ -8,6 +8,8 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 const ContractAddress = require("../../deployedContractAddress.json");
 const SIMTokenizationJSON = require("../../artifacts/contracts/SIMTokenization.sol/SIMTokenization.json");
+import TechCardsComponent from "../components/dashboard/tech-cards";
+import BannerComponent from "../components/dashboard/banner";
 
 export const injected = new InjectedConnector({});
 
@@ -34,68 +36,40 @@ const Home: NextPage = () => {
     const contract = new ethers.Contract(contractAddress, abiData, signer);
     try {
       window.alert(`Contract Address: ${contractAddress}`);
-      window.alert(`Total Contract's Token Supply: ${await contract.totalSupply()}`);
-    } catch (error){
+      window.alert(
+        `Total Contract's Token Supply: ${await contract.totalSupply()}`
+      );
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>eSIM Blockchain Tokenization</title>
         <meta name="description" content="A PoC project of Brain Station 23" />
         <link rel="icon" href="/Brainstation23.ico" />
       </Head>
-
+      <BannerComponent />
+      <TechCardsComponent />
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to eSIM Dapp</h1>
-
-        <p className={styles.description}>
-          Global eSIM DAO and IoT ownership manaement in{" "}
-          <code className={styles.code}>Blockchain</code>
-        </p>
-
         <div className={styles.grid}>
           {active ? (
-          <button onClick={execute} className={styles.card}>
-            <h2>Wallet Connected &#8383;</h2>
-            <p>Your client application has been connected with your MetaMask Wallet</p>
-          </button>
+            <button onClick={execute} className={styles.card}>
+              <h2>Wallet Connected &#8383;</h2>
+              <p>
+                Your client application has been connected with your MetaMask
+                Wallet
+              </p>
+            </button>
           ) : (
-          <button onClick={connectMetaMask} className={styles.card}>
-            <h2>Connect Wallet 	&#128423;</h2>
-            <p>Get Connected with your MetaMask Wallet.</p>
-          </button>
+            <button onClick={connectMetaMask} className={styles.card}>
+              <h2>Connect Wallet &#128423;</h2>
+              <p>Get Connected with your MetaMask Wallet.</p>
+            </button>
           )}
-
-          <a href="/create-mno" className={styles.card}>
-            <h2>Add Organization &rarr;</h2>
-            <p>Add new Organizations for operating in the DAO system</p>
-          </a>
-
-          <a href="#" className={styles.card}>
-            <h2>MNO Profile &#128390;</h2>
-            <p>
-              Discover and deploy new Mobile-Network-Operator and MNO
-              Communication Profiles.
-            </p>
-          </a>
-
-          <a href="https://www.gsma.com/" className={styles.card}>
-            <h2>GSMA &#128385;</h2>
-            <p>Representing the worldwide mobile communications industry.</p>
-          </a>
-
-          <a href="/api-doc" className={styles.card}>
-            <h2>API Doc &#128462;</h2>
-            <p>
-              Swagger helps users build, document, test and consume RESTful web services. 
-              It can be used with both a top-down and bottom-up API development approach. 
-            </p>
-          </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://brainstation-23.com/?bc"
@@ -104,7 +78,12 @@ const Home: NextPage = () => {
         >
           Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/bs23.svg" alt="Brain Station 23 Logo" width={72} height={16} />
+            <Image
+              src="/bs23.svg"
+              alt="Brain Station 23 Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
