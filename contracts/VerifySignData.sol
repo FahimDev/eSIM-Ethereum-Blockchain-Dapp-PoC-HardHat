1ef9183cc479380d7889ad034c6d362e5e6bede6
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-10-31 16:02:33 +0600

    34-Solidity MetaMask Sign Validation in Progress [part-1]
    Issue : Recover Signer Address is not valid.
    Sample output :  0xBbFdDB22A0Aed65e64107FC56c34f6d27b58D703
    Ref: [1] https://blog.chainsafe.io/how-to-verify-a-signed-message-in-solidity-6b3100277424
        [2] https://solidity-by-example.org/signature/
    Unit Test Code:
        it("SHOULD GET HASH", async function () {
          TEST = await ethers.getContractFactory("VerifySignData");
          test = await TEST.deploy();
          const _signature = "0x2baaecc1f181305791a17f18e26dba0940cb84e9c462e7c850e3a793dad08ac334ca7cb436e10d211520643bc3fbffd79dd5b1523670d452faf7510e328c92271b";
          const ownerBalance = await test.station(_signature);
          console.log("Test Signer ADDRESS: ", ownerBalance);
        });

---
+++ contracts/VerifySignData.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract VerifySignData {
+
+    function station (bytes memory _hashedMessage) public pure returns (address) {
+
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_hashedMessage);
+
+        address signer = VerifyMessage(_hashedMessage, v, r, s);
+
+        return signer;
+
+    }
+
+    function VerifyMessage(bytes memory _hashedMessage, uint8 _v, bytes32 _r, bytes32 _s) public pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _hashedMessage));
+        address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
+        return signer;
+    }
+
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+}
