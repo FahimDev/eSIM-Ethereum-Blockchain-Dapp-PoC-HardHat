6a64c56ec7a7bb9ad4241f60acb9d5ea41f23d89
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-05-10 18:43:56 +0600

    45-New `Service` Created for API delete exception handling

---
+++ administrator/core/services/exceptions.py
@@ -0,0 +1,9 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from django.utils.translation import gettext_lazy as _  # For language Localization
+
+
+class NotDeletable(APIException):
+    status_code = status.HTTP_424_FAILED_DEPENDENCY
+    default_detail = _('It has some dependent entry in db')
+    default_code = 'not deletable'
