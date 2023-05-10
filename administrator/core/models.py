8f8b02e66f748fb2dad01998faab5bb264b27700
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-05-10 18:41:01 +0600

    45-Model Updated with self-updating `created` and `modified` fields.
    New Package Installed: django-model-utils
    Ref: https://pypi.org/project/django-model-utils/

--- administrator/core/models.py
+++ administrator/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from model_utils import FieldTracker
+from model_utils.models import TimeStampedModel
+from django.db import IntegrityError
+
 
 # Create your models here.
+class BaseTimeStampedModel(TimeStampedModel):
+    tracker = FieldTracker()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, **kwargs):
+        try:
+            super().delete(**kwargs)
+        except IntegrityError:
+            return 'It has some dependent entry in db'
