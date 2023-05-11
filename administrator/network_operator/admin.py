d11be28717687310d7a4a9259891ebf7177657f9
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-05-11 16:04:42 +0600

    45-Network Operators is visible at Admin panel

--- administrator/network_operator/admin.py
+++ administrator/network_operator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from network_operator.models import NetworkOperators
+
 
 # Register your models here.
+@admin.register(NetworkOperators)
+class NetworkOperatorsAdmin(admin.ModelAdmin):
+    list_display = ('prefix', 'title')
