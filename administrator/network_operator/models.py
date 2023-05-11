34cbc497f3339abc17d16cddba674402bc1c1dc4
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-05-11 15:59:47 +0600

    45-NetworkOperators Model Created and Migrated

--- administrator/network_operator/models.py
+++ administrator/network_operator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core.models import BaseTimeStampedModel
+
 
 # Create your models here.
+class NetworkOperators(BaseTimeStampedModel):
+    title = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    network = models.CharField(max_length=100)
+    prefix = models.PositiveIntegerField()
+    mcc = models.PositiveIntegerField()
+    mnc = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('-id',)
+
+    def __str__(self) -> str:
+        return self.title[:70]
