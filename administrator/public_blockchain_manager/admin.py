c40b4cdb6eabf9e6f05dcc2228304ffe93e41bc3
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-05-11 20:35:54 +0600

    45-Model visibility added at Django Admin

--- administrator/public_blockchain_manager/admin.py
+++ administrator/public_blockchain_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from public_blockchain_manager.models import SmartContractDeployment, SignTypeData, EthSignature
+
 
 # Register your models here.
+@admin.register(SmartContractDeployment)
+class SmartContractDeploymentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'address', 'network')
+    
+
+@admin.register(SignTypeData)
+class SignTypeDataAdmin(admin.ModelAdmin):
+    list_display = ('domain_name', 'domain_version')
+    
+    
+@admin.register(EthSignature)
+class EthSignatureAdmin(admin.ModelAdmin):
+    list_display = ('signer_address', 'status')
+
